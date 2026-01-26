e98476a99c5b776ad9113ad7eed3d96c8cd44011
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-01-25 23:20:49 -0800

    coding

--- script.js
+++ script.js
@@ -4,5 +4,71 @@
 
 //----------------chapter 2, Variables for Strings----------------//
 
+// var thanks = "Thanks your for input"
 
+// var thanks = "Thanks your for input thanks you soo much"
+
+// alert(thanks)
+
+//----------------chapter 2, Variables for number----------------//
+
+// var num = 150;
+
+// num + 50;
+
+// var num2 = 150;
+
+// var newNum = num2 + 200;
+
+// console.log(newNum)
+
+// var originalNum = 23;
+// var numToBeAdded = 10;
+// var newNum = originalNum + numToBeAdded;
+
+// console.log(newNum)
+
+
+//----------------chapter 2, Variable Names Legal and Illegal----------------//
+
+
+
+
+//----------------chapter 2, Math expressions Familiar operators----------------//
+
+// var num = 1 - 2;
+
+// console.log(num)
+
+// alert(2 + 2);
+
+// var num1 = 1;
+
+// var newNum = ++num1;
+
+// console.log(newNum);
+
+// var number = ((2 * 4) * 4) + 2;
+
+// console.log(number)
+
+// var newNum = 8 * 4;
+
+// console.log(newNum)
+
+
+//----------------chapter 2, Concatenating text strings----------------//
+
+// alert("Hi my name is abdul rafay")
+
+// var intro = "Hi my name is abdul rafay"
+
+// prompt(intro)
+
+// var test = prompt("hi kya hall hai","rafay")
+
+// console.log(test)
+
+
+var smg = "chal raha hai"
 
